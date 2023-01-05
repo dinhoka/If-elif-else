db0b05ad01bc6f2ebb7c6416390a74050f1dfbae
Amanda Amaral <54366454+dinhoka@users.noreply.github.com>
2023-01-05 14:45:08 -0300

    Update if.py

--- if.py
+++ if.py
@@ -5,7 +5,9 @@ entrada = input('Você quer "entrar "ou "sair"do sistema?')
 
 
 if entrada == 'entrar':
-    print('Você entrou no sistema')
+    print('Você entrou no sistema') #digitanto 'entrar' vai aparecer a seguinte frase 'Você entrou no sistema'  IF = SE
+    
 elif entrada == 'sair':
-    print('Você saiu do sistema')
-else: print('Não entendi o que você quer')
+    print('Você saiu do sistema') # Caso digite 'sair' vai aparecer esta outra fase 'Você saiu do sistema' ELIF = se não se
+    
+else: print('Não entendi o que você quer') # como não foi nenhuma das opçoes acima caiu na oppção ELSE = SE NÃO 'não entendi'
